818f9f1b11a85ed4d083e1bcce72639a69edbbad
Nick Ray <nickray22@yahoo.com>
2015-03-11 15:41:15 -0400

    Add foreign keys to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311192822) do
+ActiveRecord::Schema.define(version: 20150311193939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 20150311192822) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "farmer_id"
+    t.integer  "customer_id"
   end
 
   create_table "users", force: :cascade do |t|
