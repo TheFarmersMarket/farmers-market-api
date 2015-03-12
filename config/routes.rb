a9895b4d8d1c165ec8d798ea0e626e6aac33c8bf
Nick Ray <nickray22@yahoo.com>
2015-03-12 11:56:51 -0400

    customer routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations",
                                     sessions: "sessions",
                                     passwords: "passwords" }
-  resources :customers
+  resources :customers, :only => [:edit, :show, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
