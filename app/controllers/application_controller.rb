a84c5bff21731ca7ff9ecac05b0238baf6fa57b7
Nick Ray <nickray22@yahoo.com>
2015-03-13 11:57:43 -0400

    auth-token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def authenticate_user_from_token!
-    user_token = params[:auth_token].presence
+    user_token = request.headers['auth-token']
+    user_token ||= params[:auth_token].presence
     user       = user_token && User.find_by_authentication_token(user_token)
 
     if user
