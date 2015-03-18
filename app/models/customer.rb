98ad78d630b205453abc2f7143e8e41642f004f8
Nick Ray <nickray22@yahoo.com>
2015-03-18 10:00:28 -0400

    basic paperclip

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ActiveRecord::Base
   belongs_to :user
   has_many :orders
   has_many :farmers, through: :orders
+
+  has_attached_file :avatar, styles: { :thumb => "200x200", :medium => "300x300" }
+
+  validates_attachment :avatar, content_type: { :content_type => "image/jpeg"},
+        size: { :in => 0..1.megabytes }
 end
--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -3,4 +3,9 @@ class Farmer < ActiveRecord::Base
   has_many :crops
   has_many :orders
   has_many :customers, through: :orders
+
+  has_attached_file :avatar, styles: { :thumb => "200x200", :medium => "300x300" }
+
+  validates_attachment :avatar, content_type: { :content_type => "image/jpeg"},
+        size: { :in => 0..1.megabytes }
 end
