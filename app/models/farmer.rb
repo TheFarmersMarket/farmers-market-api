210b4cd520504aff13abcd9631aae499b38c9c7c
Nick Ray <nickray22@yahoo.com>
2015-03-19 09:55:43 -0400

    fixes

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -7,7 +7,7 @@ class Farmer < ActiveRecord::Base
   multisearchable :against => [:business_phone, :farm, :crop_names, :contact_name]
   has_attached_file :avatar, :styles => { :large => "800x800", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
-  validates_with AttachmentSizeValidator, :attributes => :profile, :less_than => 2.megabytes
+  validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 2.megabytes
   validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
 end
