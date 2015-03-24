783d12743e4898390f01d3f026f6be4c90ee5d3f
Alan Matthews <matthewsa1320@gmail.com>
2015-03-24 19:33:29 -0400

    editing seeds.rb with new methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,35 +9,47 @@
 require "faker"
 include Faker
 
-50.times do 
-  crop = Crop.create(
-    :price => Commerce.price,
-    :currency => "$USD",
-    :quantity => Number.number(3), 
-    :farmer_id => rand(1..50)
-    )
-
-  farmer = Farmer.create(
+def create_farmer
+  @user = User.create({
+    :email => Internet.email,
+    :password => "password",
+    :profile_type => "farmer"
+    })
+  @user.farmer = Farmer.create({
     :business_phone => PhoneNumber.phone_number,
     :farm => Company.name,
     :crop_names => "soy, wheat, peaches, peanuts, pecans",
     :location => Address.city,
     :contact_name => Name.name,
     :user_id => rand(1..50)
-    )
+    })
+end
 
-  customer = Customer.create(
+def create_customer
+  @user = User.create({
+    :email => Internet.email,
+    :password => "password",
+    :profile_type => "customer"
+    })
+  @user.customer = Customer.create({
     :business => Company.name,
     :business_phone => PhoneNumber.phone_number,
     :location => Address.city,
     :contact_name => Name.name,
     :user_id => rand(1..50)
-    )
+    })
+end
 
-  user = User.create(
-    :email => Internet.email,
-    :password => "password",
-    :profile_type => "customer"
+30.times { create_farmer }
+30.times { create_customer }
+
+
+30.times do 
+  crop = Crop.create(
+    :price => Commerce.price,
+    :currency => "$USD",
+    :quantity => Number.number(3), 
+    :farmer_id => rand(1..50)
     )
 end
 
