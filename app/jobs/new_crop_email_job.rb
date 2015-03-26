715379702f291d25814378ed9180cf2a82b7e02f
Nick Ray <nickray22@yahoo.com>
2015-03-25 22:15:37 -0400

    fix bug

--- app/jobs/new_crop_email_job.rb
+++ app/jobs/new_crop_email_job.rb
@@ -1,8 +1,8 @@
 class NewCropEmailJob < ActiveJob::Base
   queue_as :default
 
-  def perform(@followers)
-    @followers.each do |f|
+  def perform(followers)
+    followers.each do |f|
     FollowerMailer.new_crop_mail(f).deliver_now
     end
   end
